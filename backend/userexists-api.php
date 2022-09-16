196003415d9ee0c1a5bb9cd4a19fe1f9f3685bb3
Mohamad Nassar <mnassar57@gmail.com>
2022-09-17 00:52:18 +0300

    response got from postman

--- backend/userexists-api.php
+++ backend/userexists-api.php
@@ -1,13 +1,11 @@
 <?php
 
     include("connection.php");
-    if(isset($_POST['username']) || isset($_POST['email'])){
-
+    if(isset($_POST['username'])){
         extract($_POST);
-        
-        echo $username;
-        $query = $mysqli->prepare("SELECT * FROM = ?");
-        $query->bind_param("s", $table);
+        //echo $username;
+        $query = $mysqli->prepare("SELECT * FROM users WHERE username = ?");
+        $query->bind_param("s", $username);
         $query->execute();
         $array = $query->get_result();
     
@@ -17,6 +15,23 @@
             $response[] = $a;
         }
     
+        $json = json_encode($response);
+        echo $json;
+    }
+
+    if(isset($_POST['email'])){
+        extract($_POST);
+        $query = $mysqli->prepare("SELECT * FROM users WHERE email = ?");
+        $query->bind_param("s", $email);
+        $query->execute();
+        $result = $query->get_result();
+        $response = [];
+        if($result->fetch_assoc()){
+            $response["success"] = true;
+        }
+        else{
+            $response["success"] = false;
+        }
         $json = json_encode($response);
         echo $json;
     }
