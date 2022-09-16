4e3ff20fe5540763d45e89d364dd093ea411cfb6
Mohamad Nassar <mnassar57@gmail.com>
2022-09-16 19:00:51 +0300

    login api frontend implementation

--- backend/select-api.php
+++ backend/select-api.php
@@ -1,19 +1,24 @@
 <?php
 
     include("connection.php");
-    $table = $_GET['table'];
-    $query = $mysqli->prepare("SELECT * FROM ?");
-    $query->bind_param("s", $table);
-    $query->execute();
-    $array = $query->get_result();
-
-    $response = [];
-
-    while($a = $array->fetch_assoc()){
-        $response[] = $a;
+    if(isset($_GET['table'])){
+        $table = $_GET['table'];
+        
+        echo $table;
+        $query = $mysqli->prepare("SELECT * FROM = ?");
+        $query->bind_param("s", $table);
+        $query->execute();
+        $array = $query->get_result();
+    
+        $response = [];
+    
+        while($a = $array->fetch_assoc()){
+            $response[] = $a;
+        }
+    
+        $json = json_encode($response);
+        echo $json;
     }
 
-    $json = json_encode($response);
-    echo $json;
     
 ?>
--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -4,12 +4,18 @@ let signup_container = document.getElementById("signup-popup");
 //Login form buttons
 let login_btn_popup = document.getElementById("login-btn-popup");
 let close_btn = document.getElementById("close-btn");
+let login_btn = document.getElementById("login-btn");
 //Signup form buttons
 let signup_btn_popup = document.getElementById("signup-btn-popup");
 let close_signup_btn_popup = document.getElementById("close-btn-signup");
+//Document values
+let username = document.getElementById("username");
+let password = document.getElementById("password");
+
 
 login_btn_popup.addEventListener("click", loginPopup);
 close_btn.addEventListener("click", closeLoginPopup);
+login_btn.addEventListener("click", loginUser);
 signup_btn_popup.addEventListener("click", e => {signup_container.classList.remove("hidden")})
 close_signup_btn_popup.addEventListener("click", e => {signup_container.classList.add("hidden")})
 
@@ -19,4 +25,20 @@ function loginPopup(){
 
 function closeLoginPopup(){
     login_container.classList.add("hidden");
+}
+
+function loginUser(){
+    let data = {"username": username.value,
+                "password": password.value};
+    url = "../backend/checkuser-api.php";
+    sendLoginRequest(url, data);
+}
+
+function sendLoginRequest(url, data){
+    stringyfiedData = JSON.stringify(data);
+	console.log();
+	fetch(url , {
+        method: 'POST',
+        body: new URLSearchParams(data),
+    }).then(response => response.json()).then(dataResponse => {checkLogin(dataResponse, data.username, data.password);});
 }
