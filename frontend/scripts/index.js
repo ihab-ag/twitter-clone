75766992f27db39d802c8c56f02d8d358de6685a
Mohamad Nassar <mnassar57@gmail.com>
2022-09-16 18:23:26 +0300

    adding buttons

--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -1,9 +1,17 @@
+//Containers
 let login_container = document.getElementById("login-popup");
+let signup_container = document.getElementById("signup-popup");
+//Login form buttons
 let login_btn_popup = document.getElementById("login-btn-popup");
 let close_btn = document.getElementById("close-btn");
+//Signup form buttons
+let signup_btn_popup = document.getElementById("signup-btn-popup");
+let close_signup_btn_popup = document.getElementById("close-btn");
 
 login_btn_popup.addEventListener("click", loginPopup);
 close_btn.addEventListener("click", closeLoginPopup);
+signup_btn_popup.addEventListener("click", e => {signup_container.classList.remove("hidden")})
+close_signup_btn_popup.addEventListener("click", e => {signup_container.classList.add("hidden")})
 
 function loginPopup(){
     login_container.classList.remove("hidden");
