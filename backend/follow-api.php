3bd8298306cad448c46f9351d119ce81ed2b84cd
Mohamad Nassar <mnassar57@gmail.com>
2022-09-18 00:41:24 +0300

    added conditions for follow/unfollow and testing

--- backend/follow-api.php
+++ backend/follow-api.php
@@ -6,12 +6,20 @@
 
     if(isset($_POST['userid']) && isset($_POST['tofollowuserid']) && isset($_POST['todo'])){
         extract($_POST);
-
-        $query = $mysqli->prepare("INSERT INTO `followers` (`user_id`, `followed_user_id`) VALUES (?,?)");
-        $query->bind_param("ii", $userid, $tofollowuserid);
-        $query->execute();
-        $result = $query->get_result();
-        $response['nowfollowing'] = true;
+        if($todo == "follow"){
+            $query = $mysqli->prepare("INSERT INTO `followers` (`user_id`, `followed_user_id`) VALUES (?,?)");
+            $query->bind_param("ii", $userid, $tofollowuserid);
+            $query->execute();
+            $result = $query->get_result();
+            $response['nowfollowing'] = true;
+        }
+        else if($todo == "unfollow"){
+            $query = $mysqli->prepare("DELETE FROM followers WHERE `followers`.`user_id` = ? AND `followers`.`followed_user_id` = ?");
+            $query->bind_param("ii", $userid, $tofollowuserid);
+            $query->execute();
+            $result = $query->get_result();
+            $response['nowunfollowed'] = true;
+        }
     }
     $json = json_encode($response);
     echo $json;
