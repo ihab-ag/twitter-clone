d4496e14c4762faac063ded6cb0f7dad6eed52ec
Mohamad Nassar <mnassar57@gmail.com>
2022-09-17 20:19:53 +0300

    getting tweet info

--- frontend/scripts/newtweet.js
+++ frontend/scripts/newtweet.js
@@ -1,10 +1,12 @@
 let tweetInput = document.getElementById("tweetInput");
 let tweetBtn = document.getElementById("new-tweet-btn");
-
+let tweetPic = document.getElementById("img-input"); 
 tweetBtn.addEventListener("click", e => {
-    console.log(tweetInput.value);
-    console.log(JSON.parse(localStorage.getItem("loggedin")).userid);
-    console.log();
+    let tweet_text = tweetInput.value;
+    let userid = JSON.parse(localStorage.getItem("loggedin")).userid;
+    let hasimage = tweetPic.files.length >= 1;
+
+    console.log(tweet_text, userid, hasimage);
 })
 
 
