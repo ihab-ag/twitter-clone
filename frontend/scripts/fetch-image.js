809b1faa9c044828728d2b3e575bb8181853cfa2
Mohamad Nassar <mnassar57@gmail.com>
2022-09-17 17:09:09 +0300

    fetching image 64 base

--- frontend/scripts/fetch-image.js
+++ frontend/scripts/fetch-image.js
@@ -6,7 +6,21 @@ fileInput.addEventListener("change", e => {
 
     reader.addEventListener("load", () => {
         console.log(reader.result);
+        let url = '../backend/addimage-api.php';
+        let data = {'image64base': reader.result,
+                    'profilepic': false,
+                    'filename': 'name'};
     })
 
     reader.readAsDataURL(file);
 })
+
+
+function sendImageRequest(url, data){
+    stringyfiedData = JSON.stringify(data);
+	console.log();
+	fetch(url , {
+        method: 'POST',
+        body: new URLSearchParams(data),
+    }).then(response => response.json()).then(dataResponse => {});
+}
