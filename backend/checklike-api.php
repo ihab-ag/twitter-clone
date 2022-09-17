95f6e40c5a261552e0fd19d5fb3ce942c2cc1985
Mohamad Nassar <mnassar57@gmail.com>
2022-09-18 01:56:27 +0300

    tweet check like api tested

--- backend/checklike-api.php
+++ backend/checklike-api.php
@@ -4,8 +4,22 @@
     header('Content-Type: application/json');
     include("connection.php");
     
-    if(isset($_POST['tweetID']) && isset($_POST['userID'])){
+    if(isset($_POST['tweet_id']) && isset($_POST['userid'])){
         extract($_POST);
+        
+        $query = $mysqli->prepare("SELECT * FROM `liked_tweets` WHERE liked_tweets.user_id = ? AND liked_tweets.tweet_id = ?");
+        $query->bind_param("ii", $userid, $tweet_id);
+        $query->execute();
+        $result = $query->get_result();
+
+        $response = [];
+        if($result->fetch_assoc()){
+            $response["liked"] = true;
+        }
+        else{
+            $response["liked"] = false;
+        }
+        
         $json = json_encode($response);
         echo $json;
     }
