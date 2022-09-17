be8547168bcba94dd1c662014990ba7c50294c87
Mohamad Nassar <mnassar57@gmail.com>
2022-09-17 20:14:39 +0300

    getting tweet text and user id

--- frontend/scripts/newtweet.js
+++ frontend/scripts/newtweet.js
@@ -3,6 +3,8 @@ let tweetBtn = document.getElementById("new-tweet-btn");
 
 tweetBtn.addEventListener("click", e => {
     console.log(tweetInput.value);
+    console.log(JSON.parse(localStorage.getItem("loggedin")).userid);
+    console.log();
 })
 
 
