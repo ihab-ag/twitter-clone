62866d9737279ecad6983c2538616419c698d9ff
Mohamad Nassar <mnassar57@gmail.com>
2022-09-18 00:38:12 +0300

    follow api implemented

--- backend/follow-api.php
+++ backend/follow-api.php
@@ -1,12 +1,18 @@
 <?php
 
-    $query = "INSERT INTO `followers` (`user_id`, `followed_user_id`) VALUES ('1', '3')";
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+    include("connection.php");
 
     if(isset($_POST['userid']) && isset($_POST['tofollowuserid']) && isset($_POST['todo'])){
         extract($_POST);
 
-        if($todo == "follow"){
-            echo "do this";
-        }
+        $query = $mysqli->prepare("INSERT INTO `followers` (`user_id`, `followed_user_id`) VALUES (?,?)");
+        $query->bind_param("ii", $userid, $tofollowuserid);
+        $query->execute();
+        $result = $query->get_result();
+        $response['nowfollowing'] = true;
     }
+    $json = json_encode($response);
+    echo $json;
 ?>
