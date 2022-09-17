e79ef646b6a700e22ba387119d31c11ebd660cd2
Mohamad Nassar <mnassar57@gmail.com>
2022-09-17 19:25:24 +0300

    showing tweet blocks working adding localstorage

--- frontend/scripts/showtweets.js
+++ frontend/scripts/showtweets.js
@@ -1,5 +1,6 @@
 const tweetsContainer = document.getElementById("main");
 const api_url = "../backend/select_tweets-api.php";
+const userid = localStorage.getItem("loggedIn").username;
 
 function sendPostRequest(url, data){
     stringyfiedData = JSON.stringify(data);
@@ -16,6 +17,7 @@ function displayTweets(dataResponse){
     for(let i = 0; i < dataResponse.length; i++){
         console.log(dataResponse[i].tweet_text);
 
+        //Tweet containers
         let tweetContainer = document.createElement('div');
         tweetContainer.classList.add("tweet", "container");
         tweetsContainer.appendChild(tweetContainer);
@@ -24,32 +26,28 @@ function displayTweets(dataResponse){
         tweetContent.classList.add("tweet__content");
         tweetContainer.appendChild(tweetContent);
 
+        //Picture
+        let tweetPicture = document.createElement('div');
+        tweetPicture.classList.add("profile-icon");
+        tweetContent.appendChild(tweetPicture);
+
+        let tweetImg = document.createElement('img');
+        tweetImg.setAttribute("src", 'content/profile-mock.jpg');
+        tweetPicture.appendChild(tweetImg);
+
+        //Tweet text
         let tweetText = document.createElement('div');
         tweetText.classList.add("tweet__texts");
         tweetContent.appendChild(tweetText);
+
+        let tweetN = document.createElement('p');
+        tweetN.classList.add("medium", "bold");
+        tweetN.innerHTML = dataResponse[i].username;
+        tweetText.appendChild(tweetN);
 
         let tweetP = document.createElement('p');
         tweetP.classList.add("medium", "normal");
         tweetP.innerHTML = dataResponse[i].tweet_text;
         tweetText.appendChild(tweetP);
     }
-
-    /*
-
-    <div class="tweet container" id="tweet-container">
-        <div class="tweet__content">
-            <div class="profile-icon">
-                <img src="content/profile-mock.jpg" alt="profile img">
-            </div>
-            <div class="tweet__texts">
-                <p class="bold medium">Name <span class="small light light-grey">@usernsme</span></p>
-                <p class="medium normal">Something randomnbeing said on twitter</p>
-            </div>
-        </div>
-        <div class="like-icon">
-            <img src="content/like.png" alt="like">
-        </div>
-    </div>
-
-    */
 }
