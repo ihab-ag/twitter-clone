35ff9e9a6bb760931cf60512f86041531bcb0339
Mohamad Nassar <mnassar57@gmail.com>
2022-09-17 20:11:36 +0300

    api working through postman

--- backend/addtweet-api.php
+++ backend/addtweet-api.php
@@ -5,7 +5,12 @@
     include("connection.php");
     if(isset($_POST['tweet_text']) && isset($_POST['userid']) && isset($_POST['hasimage'])){
         extract($_POST);
-        
+        $query = $mysqli->prepare("INSERT INTO `tweets` (`users_id`, `tweet_text`, `has_picture`) 
+        VALUES (?, ?, ?)");
+        $query->bind_param("sss", $userid, $tweet_text, $hasimage);
+        $query->execute();
+        $result = $query->get_result();
+        $response['tweetadded'] = true;
     }
     $json = json_encode($response);
     echo $json;
