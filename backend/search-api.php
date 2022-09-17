d2bc7a052e847d1e50612cf0d3de542a3050ebed
Mohamad Nassar <mnassar57@gmail.com>
2022-09-17 21:24:24 +0300

    search api working through postman

--- backend/search-api.php
+++ backend/search-api.php
@@ -6,6 +6,23 @@
     if(isset($_POST['username'])){
         $username = $_POST['username'];
         echo $username;
+        $username = $_POST['username'];
+    
+        $query = $mysqli->prepare("SELECT * FROM `users` WHERE username = ?");
 
+        $query->bind_param("s", $username);
+        $query->execute();
+        $result = $query->get_result();
+        
+        $response = [];
+        if($result->fetch_assoc()){
+            $response["found"] = true;
+        }
+        else{
+            $response["found"] = false;
+        }
+        
+        $json = json_encode($response);
+        echo $json;
     }
 ?>
