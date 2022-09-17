c4e4f8908444927afd5b7a6f0a7efab95eb75325
Mohamad Nassar <mnassar57@gmail.com>
2022-09-17 19:58:35 +0300

    adding new tweet functionality

---
+++ frontend/scripts/newtweet.js
@@ -0,0 +1,8 @@
+let tweetInput = document.getElementById("tweetInput");
+let tweetBtn = document.getElementById("new-tweet-btn");
+
+tweetBtn.addEventListener("click", e => {
+    console.log(tweetInput.value);
+})
+
+
