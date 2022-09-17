6a3600a63dc568514de30a0035288df7467ab0c2
Mohamad Nassar <mnassar57@gmail.com>
2022-09-18 01:39:58 +0300

    updating tweet selection query

--- backend/select_tweets-api.php
+++ backend/select_tweets-api.php
@@ -2,8 +2,11 @@
 
     include("connection.php");
     if(isset($_POST['userid'])){
-        $query = $mysqli->prepare("SELECT * FROM `tweets` JOIN users on tweets.users_id = users.id 
-        WHERE tweets.users_id IN (SELECT followers.followed_user_id FROM followers WHERE followers.user_id = ?) OR tweets.users_id = ?");
+        $query = $mysqli->prepare("SELECT tweets.id as tweet_id, users_id, tweet_text, has_picture, users.id, username, fname, lname 
+        FROM `tweets` JOIN users on tweets.users_id = users.id 
+        WHERE tweets.users_id 
+        IN (SELECT followers.followed_user_id FROM followers WHERE followers.user_id = ?) 
+        OR tweets.users_id = ?");
         $query->bind_param("ii", $_POST['userid'], $_POST['userid']);
         $query->execute();
         $array = $query->get_result();
@@ -12,19 +15,9 @@
     
         while($a = $array->fetch_assoc()){
             $response[] = $a;
-            //echo $a;
         }
-        foreach($response as $key => $csm)
-        {
-            $query = $mysqli->prepare("SELECT * FROM `liked_tweets` JOIN users on tweets.users_id = users.id 
-            WHERE tweets.users_id IN (SELECT followers.followed_user_id FROM followers WHERE followers.user_id = ?) OR tweets.users_id = ?");
-            $query->bind_param("ii", $_POST['userid'], $_POST['userid']);
-            $query->execute();
-            $array = $query->get_result();
-            $response[$key]['liked'] = true;
-        }
-        print_r($response);
+        //print_r($response);
         $json = json_encode($response);
-        //echo $json;
+        echo $json;
     }
 ?>
