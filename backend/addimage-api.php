a0ca0d37c7ec64169da2852f49ed67a382270dea
Mohamad Nassar <mnassar57@gmail.com>
2022-09-17 17:00:21 +0300

    add image working version

--- backend/addimage-api.php
+++ backend/addimage-api.php
@@ -1,16 +1,17 @@
 <?php
 
     $output_file = "./";
-    if(isset($_POST['image64base']) && isset($_POST['profilepic'])){
-
-        $base64_string = "data:image/png;base64,";
-        $base64_string .= $_POST['image64base'];
+    if(isset($_POST['image64base']) && isset($_POST['profilepic']) && isset($_POST['name'])){
+        extract($_POST);
+        $base64_string = $image64base;
+        echo $base64_string;
         $decoder = base64_decode($base64_string);
         $img = imagecreatefromstring($decoder);
 
         if($img){
             echo 'worked';
-            imagejpeg($img, "./file.jpg");
+            imagejpeg($img, "./" . $name . ".jpg");
+            //query to insert
         }
     }
     
