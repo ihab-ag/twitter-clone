9c6778d1796fb6aaa2127894aaf8e7e82fe20a65
Mohamad Nassar <mnassar57@gmail.com>
2022-09-17 14:37:54 +0300

    check if user is following api

---
+++ backend/checkfollowing-api.php
@@ -0,0 +1,15 @@
+<?php
+
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+    include("connection.php");
+    
+    if(isset($_POST['username']) && isset($_POST['tofollowusername'])){
+        $username = $_POST['username'];
+        $tofollowusername = $_POST['tofollowusername'];
+    
+        $query = $mysqli->prepare("SELECT * FROM `followers` WHERE followers.user_id = (SELECT users.id FROM users WHERE username = ?)
+                                    AND followers.followed_user_id = (SELECT users.id FROM users WHERE username = ?)");
+
+    }
+?>
