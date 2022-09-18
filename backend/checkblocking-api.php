fe8ae4428530c3e5bb70da4438c6c0cf233378e2
Mohamad Nassar <mnassar57@gmail.com>
2022-09-18 09:45:52 +0300

    api postman testing done

--- backend/checkblocking-api.php
+++ backend/checkblocking-api.php
@@ -3,11 +3,25 @@
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json');
     include("connection.php");
-    
-    if(isset($_POST['userid']) && isset($_POST['toblockuser'])){
+    $response = [];
+    if(isset($_POST['userid']) && isset($_POST['toblockuserid'])){
         $userid = $_POST['userid'];
-        $toblockuser = $_POST['toblockuser'];
+        $toblockuserid = $_POST['toblockuserid'];
     
-
+        $query = $mysqli->prepare("SELECT * FROM `blocked_users` WHERE userid  = ? AND blockinguserid  = ?");
+        $query->bind_param("ss", $userid, $toblockuserid);
+        $query->execute();
+        $result = $query->get_result();
+        
+        $response = [];
+        if($result->fetch_assoc()){
+            $response["blocking"] = true;
+        }
+        else{
+            $response["blocking"] = false;
+        }
+        //$response[]
+        $json = json_encode($response);
+        echo $json;
     }
 ?>
